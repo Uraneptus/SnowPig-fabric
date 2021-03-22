f8bfb552856ac82b0029d99a550517055fbbe824
Uraneptus <50745606+Uraneptus333@users.noreply.github.com>
2021-03-22 16:07:37 +0100

    Update SnowPig.java

--- src/main/java/net/uraneptus/snowpig/SnowPig.java
+++ src/main/java/net/uraneptus/snowpig/SnowPig.java
@@ -1,11 +1,16 @@
 package net.uraneptus.snowpig;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.Biome;
 import net.uraneptus.snowpig.common.entities.SnowPigEntity;
 import net.uraneptus.snowpig.core.registry.EntityReg;
 import net.uraneptus.snowpig.core.registry.ItemReg;
 
+@SuppressWarnings("deprecation")
 public class SnowPig implements ModInitializer {
 
 	public static final String MOD_ID = "snowpig";
@@ -16,5 +21,8 @@ public class SnowPig implements ModInitializer {
 		EntityReg.registerEntityTypes();
 
 		FabricDefaultAttributeRegistry.register(EntityReg.SNOW_PIG, SnowPigEntity.createMobAttributes());
+
+		BiomeModifications.addSpawn(BiomeSelectors.categories(Biome.Category.ICY), SpawnGroup.CREATURE, EntityReg.SNOW_PIG, 20, 1, 4);
+
 	}
 }
