3133f7fb7ce35ea0012a289a994a214fe4e624d7
dayofpi <thedayofpi@gmail.com>
2021-06-13 19:53:22 -0300

    lightning fix

--- src/main/java/net/uraneptus/snowpig/common/entities/SnowPigEntity.java
+++ src/main/java/net/uraneptus/snowpig/common/entities/SnowPigEntity.java
@@ -1,29 +1,50 @@
 package net.uraneptus.snowpig.common.entities;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LightningEntity;
-import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import net.uraneptus.snowpig.core.registry.EntityReg;
 import net.uraneptus.snowpig.core.registry.SoundReg;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
-public class SnowPigEntity extends PigEntity {
+public class SnowPigEntity extends AnimalEntity implements ItemSteerable, Saddleable {
+    private static final TrackedData<Boolean> SADDLED;
+    private static final TrackedData<Integer> BOOST_TIME;
+    private static final Ingredient BREEDING_INGREDIENT;
+    private final SaddledComponent saddledComponent;
 
     public SnowPigEntity(EntityType<? extends SnowPigEntity> snowpig, World worldIn) {
         super(snowpig, worldIn);
+        this.saddledComponent = new SaddledComponent(this.dataTracker, BOOST_TIME, SADDLED);
     }
 
     public static DefaultAttributeContainer.Builder createMobAttributes() {
@@ -34,7 +55,16 @@ public class SnowPigEntity extends PigEntity {
                 .add(EntityAttributes.GENERIC_ARMOR, 0.5D);
     }
 
-    public void onStruckByLightning(ServerWorld world, LightningEntity lightning) {
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25D));
+        this.goalSelector.add(3, new AnimalMateGoal(this, 1.0D));
+        this.goalSelector.add(4, new TemptGoal(this, 1.2D, Ingredient.ofItems(Items.CARROT_ON_A_STICK), false));
+        this.goalSelector.add(4, new TemptGoal(this, 1.2D, false, BREEDING_INGREDIENT));
+        this.goalSelector.add(5, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.add(6, new WanderAroundFarGoal(this, 1.0D));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(8, new LookAroundGoal(this));
     }
 
     public static boolean canSpawn(EntityType<SnowPigEntity> entityType, ServerWorldAccess world, SpawnReason reason, BlockPos pos, Random random) {
@@ -42,23 +72,151 @@ public class SnowPigEntity extends PigEntity {
     }
 
     @Override
-    public PigEntity createChild(ServerWorld serverWorld, PassiveEntity passiveEntity) {
+    public SnowPigEntity createChild(ServerWorld serverWorld, PassiveEntity passiveEntity) {
         return EntityReg.SNOW_PIG.create(serverWorld);
+    }
+
+    public boolean isBreedingItem(ItemStack stack) {
+        return BREEDING_INGREDIENT.test(stack);
+    }
+
+    public void travel(Vec3d movementInput) {
+        this.travel(this, this.saddledComponent, movementInput);
+    }
+
+    public void setMovementInput(Vec3d movementInput) {
+        super.travel(movementInput);
+    }
+
+    public boolean consumeOnAStickItem() {
+        return this.saddledComponent.boost(this.getRandom());
+    }
+
+    public float getSaddledSpeed() {
+        return (float)this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 0.225F;
     }
 
     public SoundEvent getAmbientSound() {
         return SoundReg.SNOW_PIG_AMBIENT;
     }
-
     public SoundEvent getHurtSound(DamageSource source) {
         return SoundReg.SNOW_PIG_HURT;
     }
-
     public SoundEvent getDeathSound() {
         return SoundReg.SNOW_PIG_DEATH;
     }
-
     protected void playStepSound(BlockPos pos, BlockState state) {
         this.playSound(SoundReg.SNOW_PIG_STEP, 0.15F, 1.0F);
+    }
+
+    @Nullable
+    public Entity getPrimaryPassenger() {
+        return this.getPassengerList().isEmpty() ? null : this.getPassengerList().get(0);
+    }
+
+    public boolean canBeControlledByRider() {
+        Entity entity = this.getPrimaryPassenger();
+        if (!(entity instanceof PlayerEntity)) {
+            return false;
+        } else {
+            PlayerEntity playerEntity = (PlayerEntity)entity;
+            return playerEntity.getMainHandStack().getItem() == Items.CARROT_ON_A_STICK || playerEntity.getOffHandStack().getItem() == Items.CARROT_ON_A_STICK;
+        }
+    }
+
+    public void onTrackedDataSet(TrackedData<?> data) {
+        if (BOOST_TIME.equals(data) && this.world.isClient) {
+            this.saddledComponent.boost();
+        }
+        super.onTrackedDataSet(data);
+    }
+
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(SADDLED, false);
+        this.dataTracker.startTracking(BOOST_TIME, 0);
+    }
+
+    public void writeCustomDataToTag(CompoundTag tag) {
+        super.writeCustomDataToTag(tag);
+        this.saddledComponent.toTag(tag);
+    }
+
+    public void readCustomDataFromTag(CompoundTag tag) {
+        super.readCustomDataFromTag(tag);
+        this.saddledComponent.fromTag(tag);
+    }
+
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        boolean bl = this.isBreedingItem(player.getStackInHand(hand));
+        if (!bl && this.isSaddled() && !this.hasPassengers() && !player.shouldCancelInteraction()) {
+            if (!this.world.isClient) {
+                player.startRiding(this);
+            }
+
+            return ActionResult.success(this.world.isClient);
+        } else {
+            ActionResult actionResult = super.interactMob(player, hand);
+            if (!actionResult.isAccepted()) {
+                ItemStack itemStack = player.getStackInHand(hand);
+                return itemStack.getItem() == Items.SADDLE ? itemStack.useOnEntity(player, this, hand) : ActionResult.PASS;
+            } else {
+                return actionResult;
+            }
+        }
+    }
+
+    public boolean canBeSaddled() {
+        return this.isAlive() && !this.isBaby();
+    }
+
+    protected void dropInventory() {
+        super.dropInventory();
+        if (this.isSaddled()) {
+            this.dropItem(Items.SADDLE);
+        }
+    }
+
+    public boolean isSaddled() {
+        return this.saddledComponent.isSaddled();
+    }
+
+    public void saddle(@Nullable SoundCategory sound) {
+        this.saddledComponent.setSaddled(true);
+        if (sound != null) {
+            this.world.playSoundFromEntity(null, this, SoundEvents.ENTITY_PIG_SADDLE, sound, 0.5F, 1.0F);
+        }
+    }
+
+    public Vec3d updatePassengerForDismount(LivingEntity passenger) {
+        Direction direction = this.getMovementDirection();
+        if (direction.getAxis() != Direction.Axis.Y) {
+            int[][] is = Dismounting.getDismountOffsets(direction);
+            BlockPos blockPos = this.getBlockPos();
+            BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+            for (EntityPose entityPose : passenger.getPoses()) {
+                Box box = passenger.getBoundingBox(entityPose);
+
+                for (int[] js : is) {
+                    mutable.set(blockPos.getX() + js[0], blockPos.getY(), blockPos.getZ() + js[1]);
+                    double d = this.world.getDismountHeight(mutable);
+                    if (Dismounting.canDismountInBlock(d)) {
+                        Vec3d vec3d = Vec3d.ofCenter(mutable, d);
+                        if (Dismounting.canPlaceEntityAt(this.world, passenger, box.offset(vec3d))) {
+                            passenger.setPose(entityPose);
+                            return vec3d;
+                        }
+                    }
+                }
+            }
+        }
+        return super.updatePassengerForDismount(passenger);
+    }
+
+    static {
+        SADDLED = DataTracker.registerData(PigEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+        BOOST_TIME = DataTracker.registerData(PigEntity.class, TrackedDataHandlerRegistry.INTEGER);
+        BREEDING_INGREDIENT = Ingredient.ofItems(Items.CARROT, Items.POTATO, Items.BEETROOT);
     }
 }
