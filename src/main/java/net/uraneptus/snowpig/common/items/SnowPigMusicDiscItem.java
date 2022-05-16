525e919788c9704d30a6cf0fabe4c1b3cd1c250c
JustinPlayzz <75293234+JustinPlayzz@users.noreply.github.com>
2022-05-15 20:15:17 -0400

    Updated to 1.18.2!

---
+++ src/main/java/net/uraneptus/snowpig/common/items/SnowPigMusicDiscItem.java
@@ -0,0 +1,10 @@
+package net.uraneptus.snowpig.common.items;
+
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.sound.SoundEvent;
+
+public class SnowPigMusicDiscItem extends MusicDiscItem {
+    public SnowPigMusicDiscItem(int comparatorOutput, SoundEvent sound, Settings settings) {
+        super(comparatorOutput, sound, settings);
+    }
+}
